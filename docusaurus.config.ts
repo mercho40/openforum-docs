551e230e530372f6ac3f0031fc1ba0056f05d9d0
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-06-13 19:54:01 -0300

    algolia search implementation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import 'dotenv/config';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -72,6 +73,39 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
+
+    algolia: {
+      // The application ID provided by Algolia
+      appId: process.env.ALGOLIA_APP_ID,
+
+      // Public API key: it is safe to commit it
+      apiKey: process.env.ALGOLIA_SEARCH_KEY,
+
+      indexName: process.env.ALGOLIA_INDEX_NAME,
+
+      // Optional: see doc section below
+      // contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+      //
+      // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      // searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      // insights: false,
+
+      //... other Algolia params
+    },
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'OpenForum Docs',
